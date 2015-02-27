67bb2731d5bc116f0820995c2f3071e72618c9c9
Mike Moscardini <mmoscardini@brightcove.com>
2015-02-27 11:07:04 -0500

    Add Widevine Sample

---
+++ Widevine/BasicWidevinePlayer/objc/BasicWidevinePlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicWidevinePlayer
+//
+//  Created by Mike Moscardini on 2/26/15.
+//  Copyright (c) 2015 Brightcove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Widevine/BasicWidevinePlayer/objc/BasicWidevinePlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicWidevinePlayer
+//
+//  Created by Mike Moscardini on 2/26/15.
+//  Copyright (c) 2015 Brightcove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
