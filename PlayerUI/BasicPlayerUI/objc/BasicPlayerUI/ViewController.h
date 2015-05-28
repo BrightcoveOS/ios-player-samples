eea04f21fa198b11930223e384d391133e47228c
Mike Moscardini <mmoscardini@brightcove.com>
2015-05-28 15:22:09 -0400

    Add Basic PlayerUI sample

---
+++ PlayerUI/BasicPlayerUI/objc/BasicPlayerUI/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  BasicPlayerUI
+//
+//  Created by Mike Moscardini on 5/28/15.
+//  Copyright (c) 2015 Brightcove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
+
