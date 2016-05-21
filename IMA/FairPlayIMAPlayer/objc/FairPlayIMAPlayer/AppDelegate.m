51288df35374365e36654a27bc69152e77d35346
Steve Bushell <sbushell@brightcove.com>
2016-05-21 11:54:22 -0400

    Objective C Sample code to show FairPlay and IMA working together.

---
+++ IMA/FairPlayIMAPlayer/objc/FairPlayIMAPlayer/AppDelegate.m
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.m
+//  FairPlayIMAPlayer
+//
+//  Copyright (c) 2016 Brightcove, Inc. All rights reserved.
+//  License: https://accounts.brightcove.com/en/terms-and-conditions
+//
+
+#import "AppDelegate.h"
+
+#import <AVFoundation/AVFoundation.h>
+
+
+@implementation AppDelegate
+
+void simulatorCheck()
+{
+#if (TARGET_OS_SIMULATOR)
+
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"FairPlay Warning" message:@"FairPlay only works on actual iOS devices, not in a simulator.\n\nYou will not be able to view any FairPlay content." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alert show];
+
+#endif
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    // FairPlay doesn't work when we're running in a simulator, so put up an alert.
+    simulatorCheck();
+
+    // We need the below code in order to ensure that audio plays back when we
+    // expect it to. For example, without setting this code, we won't hear the video
+    // when the mute switch is on. For simplicity in the sample, we are going to
+    // put this in the app delegate.  Check out
+    // https://developer.apple.com/Library/ios/documentation/Audio/Conceptual/AudioSessionProgrammingGuide/Introduction/Introduction.html
+    // for more information on how to use this in your own app.
+
+    NSError *categoryError = nil;
+    BOOL success = [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:&categoryError];
+
+    if (!success)
+    {
+        NSLog(@"AppDelegate Debug - Error setting AVAudioSession category.  Because of this, there may be no sound. `%@`", categoryError);
+    }
+    
+    return YES;
+}
+
+@end
---
+++ IMA/FairPlayIMAPlayer/objc/FairPlayIMAPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FairPlayIMAPlayer
+//
+//  Copyright (c) 2014 Brightcove, Inc. All rights reserved.
+//  License: https://accounts.brightcove.com/en/terms-and-conditions
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
