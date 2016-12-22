8b0757210c5e70d4839e9056580ec93712d5c38b
Steve Bushell <sbushell@brightcove.com>
2016-12-22 15:28:08 -0500

    Sidecar: comments and code cleanup

--- SidecarSubtitles/BasicSidecarSubtitlesPlayer/objc/BasicSidecarSubtitlesPlayer/AppDelegate.m
+++ SidecarSubtitles/BasicSidecarSubtitlesPlayer/objc/BasicSidecarSubtitlesPlayer/AppDelegate.m
@@ -2,7 +2,7 @@
 //  AppDelegate.m
 //  BasicSidecarSubtitlesPlayer
 //
-//  Copyright (c) 2015 Brightcove, Inc. All rights reserved.
+//  Copyright (c) 2016 Brightcove, Inc. All rights reserved.
 //  License: https://accounts.brightcove.com/en/terms-and-conditions
 //
 
