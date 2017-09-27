a7c80c6301a20d4fcc46b07fae2667ddd122c586
Steve Bushell <sbushell@brightcove.com>
2017-09-27 11:20:52 -0400

    swift basic player updates
    removed bridging header
    import module directly in code
    deployment target is iOS 9.0
    simplified, more swifty code
    added progress delegate

--- Player/VideoCloudBasicPlayer/swift/VideoCloudBasicPlayer/ViewController.swift
+++ Player/VideoCloudBasicPlayer/swift/VideoCloudBasicPlayer/ViewController.swift
@@ -6,71 +6,63 @@
 //
 
 import UIKit
-
+import BrightcovePlayerSDK
 
 let kViewControllerPlaybackServicePolicyKey = "BCpkADawqM3n0ImwKortQqSZCgJMcyVbb8lJVwt0z16UD0a_h8MpEYcHyKbM8CGOPxBRp0nfSVdfokXBrUu3Sso7Nujv3dnLo0JxC_lNXCl88O7NJ0PR0z2AprnJ_Lwnq7nTcy1GBUrQPr5e"
 let kViewControllerAccountID = "4800266849001"
 let kViewControllerVideoID = "5255514387001"
 
-
 class ViewController: UIViewController, BCOVPlaybackControllerDelegate {
-
+    
+    let sharedSDKManager = BCOVPlayerSDKManager.shared()
     let playbackService = BCOVPlaybackService(accountId: kViewControllerAccountID, policyKey: kViewControllerPlaybackServicePolicyKey)
     let playbackController :BCOVPlaybackController
     @IBOutlet weak var videoContainerView: UIView!
-
-    required init?(coder aDecoder: NSCoder)
-    {
-        let manager = BCOVPlayerSDKManager.shared();
-        playbackController = (manager?.createPlaybackController(viewStrategy: nil))!
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        playbackController = (sharedSDKManager?.createPlaybackController())!
+        
         super.init(coder: aDecoder)
-
+        
         playbackController.delegate = self
         playbackController.isAutoAdvance = true
         playbackController.isAutoPlay = true
     }
-
-    override func viewDidLoad()
-    {
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        let controlView = BCOVPUIBasicControlView.withVODLayout()
-        let playerView = BCOVPUIPlayerView(playbackController: self.playbackController, options: nil, controlsView: controlView)
+        
+        // Set up our player view. Create with a standard VOD layout.
+        let playerView = BCOVPUIPlayerView(playbackController: self.playbackController, options: nil, controlsView: BCOVPUIBasicControlView.withVODLayout())
+        
+        // Install in the container view and match its size.
         playerView?.frame = self.videoContainerView.bounds
         playerView?.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         self.videoContainerView.addSubview(playerView!)
+        
+        // Associate the playerView with the playback controller.
         playerView?.playbackController = playbackController
-
+        
         requestContentFromPlaybackService()
     }
-
-    func requestContentFromPlaybackService()
-    {
+    
+    func requestContentFromPlaybackService() {
         playbackService?.findVideo(withVideoID: kViewControllerVideoID, parameters: nil) { (video: BCOVVideo?, jsonResponse: [AnyHashable: Any]?, error: Error?) -> Void in
             
-            if let v = video
-            {
+            if let v = video {
                 self.playbackController.setVideos([v] as NSArray)
-            }
-            else
-            {
+            } else {
                 print("ViewController Debug - Error retrieving video: \(error?.localizedDescription ?? "unknown error")")
             }
         }
     }
-
-    func playbackController(_ controller: BCOVPlaybackController!, didAdvanceTo session: BCOVPlaybackSession!)
-    {
-        NSLog("ViewController Debug - Advanced to new session.")
+    
+    func playbackController(_ controller: BCOVPlaybackController!, didAdvanceTo session: BCOVPlaybackSession!) {
+        print("Advanced to new session")
     }
-
-    // MARK: - UI Styling
-
-    override var preferredStatusBarStyle : UIStatusBarStyle
-    {
-        return .lightContent
+    
+    func playbackController(_ controller: BCOVPlaybackController!, playbackSession session: BCOVPlaybackSession!, didProgressTo progress: TimeInterval) {
+        print("Progress: \(progress) seconds")
     }
 }
-
