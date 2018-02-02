643dd874bd2f1e033a17f960e650b75f87985f91
Tim <trodgers@mac.com>
2018-02-02 13:40:53 -0500

    Removed development code.

--- Offline/OfflinePlayer/objc/OfflinePlayer/VideosViewController.m
+++ Offline/OfflinePlayer/objc/OfflinePlayer/VideosViewController.m
@@ -12,17 +12,9 @@
 
 // Dynamic Delivery account credentials
 // Your account can contain FairPlay-protected HLS videos, or unprotected HLS videos
-// TODO: BOZO
-#if 0
 NSString * const kDynamicDeliveryAccountID = @"5434391461001";
 NSString * const kDynamicDeliveryPolicyKey = @"BCpkADawqM0T8lW3nMChuAbrcunBBHmh4YkNl5e6ZrKQwPiK_Y83RAOF4DP5tyBF_ONBVgrEjqW6fbV0nKRuHvjRU3E8jdT9WMTOXfJODoPML6NUDCYTwTHxtNlr5YdyGYaCPLhMUZ3Xu61L";
 NSString * const kDynamicDeliveryPlaylistRefID = @"brightcove-native-sdk-plist";
-#else
-// Enter your Brightcove Dynamic Delivery account information here:
-NSString * const kDynamicDeliveryAccountID = @"3636334164001";
-NSString * const kDynamicDeliveryPolicyKey = @"BCpkADawqM04pilWqSIDfTmwPNiBscekcQB9wQ3IiTUZ4baBIx6LyLTrACuS0KJPOlpeEui10NOSgQ8zPTk8iSRPkQXSCGBrEqJD_4vJtX31zRvnk-4Wq8svz8j6abVYnqTijMliCf_1lIoa";
-NSString * const kDynamicDeliveryPlaylistRefID = @"offline_drm_fairplay_dash_playlist";
-#endif
 
 // For quick access to this controller from other tabs
 VideosViewController *gVideosViewController;
