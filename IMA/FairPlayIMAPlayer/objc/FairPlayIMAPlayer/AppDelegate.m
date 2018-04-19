4d6e40c84c99a219dee7d2dce27c6286e5f58d8e
Tim <trodgers@brightcove.com>
2018-04-19 15:28:34 -0400

    Replace deprecated UIAlertView with UIAlertController.

--- IMA/FairPlayIMAPlayer/objc/FairPlayIMAPlayer/AppDelegate.m
+++ IMA/FairPlayIMAPlayer/objc/FairPlayIMAPlayer/AppDelegate.m
@@ -13,21 +13,8 @@
 
 @implementation AppDelegate
 
-void simulatorCheck()
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#if (TARGET_OS_SIMULATOR)
-
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"FairPlay Warning" message:@"FairPlay only works on actual iOS devices, not in a simulator.\n\nYou will not be able to view any FairPlay content." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-    [alert show];
-
-#endif
-}
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    // FairPlay doesn't work when we're running in a simulator, so put up an alert.
-    simulatorCheck();
-
     // We need the below code in order to ensure that audio plays back when we
     // expect it to. For example, without setting this code, we won't hear the video
     // when the mute switch is on. For simplicity in the sample, we are going to
