3ee348556d72ec0fb12a7d073180480d21e9f4b3
Jeremy Blaker <jblaker@brightcove.com>
2018-10-11 10:40:05 -0400

    Added swift version of BasicOmniturePlayer sample app

---
+++ Omniture/BasicOmniturePlayer/swift/BasicOmniturePlayer/BasicOmniturePlayer-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ADBMobile.h"
+#import "ADBMediaHeartbeat.h"
+#import "ADBMediaHeartbeatConfig.h"
