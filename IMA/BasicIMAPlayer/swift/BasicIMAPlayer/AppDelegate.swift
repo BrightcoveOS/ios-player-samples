ab88ed0c312a5b62b5fb57e2559d67eb772d1d65
Jeremy Blaker <jblaker@brightcove.com>
2018-10-19 10:11:31 -0400

    Added Swift version of BasicIMAPlayer

---
+++ IMA/BasicIMAPlayer/swift/BasicIMAPlayer/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  BasicIMAPlayer
+//
+//  Copyright © 2018 Brightcove, Inc. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // We need the below code in order to ensure that audio plays back when we
+        // expect it to. For example, without setting this code, we won't hear the video
+        // when the mute switch is on. For simplicity in the sample, we are going to
+        // put this in the app delegate.  Check out https://developer.apple.com/Library/ios/documentation/Audio/Conceptual/AudioSessionProgrammingGuide/Introduction/Introduction.html
+        // for more information on how to use this in your own app.
+        
+        var categoryError :NSError?
+        var success: Bool
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .moviePlayback)
+            success = true
+        } catch let error as NSError {
+            categoryError = error
+            success = false
+        }
+        
+        if !success {
+            print("AppDelegate Debug - Error setting AVAudioSession category.  Because of this, there may be no sound. \(categoryError!)")
+        }
+        
+        return true
+    }
+
+
+}
+
