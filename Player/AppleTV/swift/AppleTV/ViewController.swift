64ca14a95939fca4b9f4981db48d3161482b32d4
Jeremy Blaker <jblaker@users.noreply.github.com>
2018-11-14 09:41:03 -0500

    Added missing closing bracket

--- Player/AppleTV/swift/AppleTV/ViewController.swift
+++ Player/AppleTV/swift/AppleTV/ViewController.swift
@@ -15,7 +15,7 @@ fileprivate struct playbackConfig {
     static let policyKey = "BCpkADawqM3n0ImwKortQqSZCgJMcyVbb8lJVwt0z16UD0a_h8MpEYcHyKbM8CGOPxBRp0nfSVdfokXBrUu3Sso7Nujv3dnLo0JxC_lNXCl88O7NJ0PR0z2AprnJ_Lwnq7nTcy1GBUrQPr5e"
     static let accountID = "4800266849001"
     static let videoID = "5754208017001"
-
+}
 
 class ViewController: UIViewController
 {
