a9ded619964c08314b4605d4c282b95b49289ff9
Jeremy Blaker <jblaker@users.noreply.github.com>
2020-01-13 15:43:56 -0500

    Updated comment about AVAudioSession (#102)

--- IMA/BasicIMAPlayer/objc/BasicIMAPlayer/AppDelegate.m
+++ IMA/BasicIMAPlayer/objc/BasicIMAPlayer/AppDelegate.m
@@ -15,9 +15,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Set the AVAudioSession category to allow audio playback in the background
-    // or when the mute button is on. Refer to the AVAudioSession Class Reference:
-    // https://developer.apple.com/documentation/avfoundation/avaudiosession
+    /*
+     Set the AVAudioSession category to allow audio playback when:
+
+     1: Silent Mode is enabled, or
+     2: When the app is in the background, and
+     2a:`allowsBackgroundAudioPlayback` is enabled on the playback controller, and/or
+     2b:`allowsExternalPlayback` is enabled on the playback controller, and
+     2c: "Audio, AirPlay, and Picture in Picture" is enabled as a Background Mode capability.
+
+     Refer to the AVAudioSession Class Reference:
+     https://developer.apple.com/documentation/avfoundation/avaudiosession
+    */
 
     NSError *categoryError = nil;
     // see https://developer.apple.com/documentation/avfoundation/avaudiosessioncategoryplayback
