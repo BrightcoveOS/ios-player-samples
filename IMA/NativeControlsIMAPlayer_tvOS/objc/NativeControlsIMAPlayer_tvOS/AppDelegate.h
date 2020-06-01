c2b5d40821d10d0645c75d83090c035a2dce21e4
Jeremy Blaker <jblaker@brightcove.com>
2020-06-01 14:19:00 -0400

    NativeControls sample for tvOS

---
+++ IMA/NativeControlsIMAPlayer_tvOS/objc/NativeControlsIMAPlayer_tvOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NativeControlsIMAPlayer_tvOS
+//
+//  Created by Jeremy Blaker on 6/1/20.
+//  Copyright © 2020 Brightcove, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
