13dd9c070083fe562446f6aef1ef7f446f5d75e6
Jeremy Blaker <jblaker@brightcove.com>
2020-07-06 13:17:23 -0400

    Brightcove Google Cast Receiver Sample Project

---
+++ GoogleCast/BrightcoveCastReceiver/objc/BrightcoveCastReceiver/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  BrightcoveCastReceiver
+//
+//  Created by Jeremy Blaker on 7/6/20.
+//  Copyright © 2020 Brightcove, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
---
+++ GoogleCast/BrightcoveCastReceiver/objc/BrightcoveCastReceiver/AppDelegate.m
@@ -0,0 +1,67 @@
+//
+//  AppDelegate.m
+//  BrightcoveCastReceiver
+//
+//  Created by Jeremy Blaker on 7/6/20.
+//  Copyright © 2020 Brightcove, Inc. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@import BrightcoveGoogleCast;
+@import GoogleCast;
+@import AVFoundation;
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    /*
+     Set the AVAudioSession category to allow audio playback when:
+
+     1: Silent Mode is enabled, or
+     2: When the app is in the background, and
+     2a:`allowsBackgroundAudioPlayback` is enabled on the playback controller, and/or
+     2b:`allowsExternalPlayback` is enabled on the playback controller, and
+     2c: "Audio, AirPlay, and Picture in Picture" is enabled as a Background Mode capability.
+
+     Refer to the AVAudioSession Class Reference:
+     https://developer.apple.com/documentation/avfoundation/avaudiosession
+    */
+    
+    NSError *categoryError = nil;
+    // see https://developer.apple.com/documentation/avfoundation/avaudiosessioncategoryplayback
+    // and https://developer.apple.com/documentation/avfoundation/avaudiosessionmodemovieplayback
+    BOOL success = [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback mode:AVAudioSessionModeMoviePlayback options:AVAudioSessionCategoryOptionDuckOthers error:&categoryError];
+    if (!success)
+    {
+        NSLog(@"AppDelegate Debug - Error setting AVAudioSession category.  Because of this, there may be no sound. `%@`", categoryError);
+    }
+    
+    // More Info @ https://developers.google.com/cast/docs/ios_sender/integrate#initialize_the_cast_context
+    GCKDiscoveryCriteria *discoveryCriteria = [[GCKDiscoveryCriteria alloc] initWithApplicationID:kBCOVCAFReceiverApplicationID];
+    GCKCastOptions *options = [[GCKCastOptions alloc] initWithDiscoveryCriteria:discoveryCriteria];
+    [GCKCastContext setSharedInstanceWithOptions:options];
+    
+    // More Info @ https://developers.google.com/cast/docs/ios_sender/integrate#add_expanded_controller
+    [GCKCastContext sharedInstance].useDefaultExpandedMediaControls = YES;
+    
+    // More Info @ https://developers.google.com/cast/docs/ios_sender/integrate#add_mini_controllers
+    UIStoryboard *appStoryboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UINavigationController *navigationController = [appStoryboard instantiateViewControllerWithIdentifier:@"NavController"];
+    GCKUICastContainerViewController *castContainerVC = [[GCKCastContext sharedInstance] createCastContainerControllerForViewController:navigationController];
+    castContainerVC.miniMediaControlsItemEnabled = YES;
+
+    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+    self.window.rootViewController = castContainerVC;
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+@end
