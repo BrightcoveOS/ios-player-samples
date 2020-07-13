dd03ca883f28cdca7675239bd268b25626f1fd3e
Carlos A. Ceja Limón <cceja@brightcove.com>
2020-07-13 15:12:14 -0500

    SLS + IMA CSAI Sample App

---
+++ SSAI/SLS_IMA-Player/objc/SLS_IMA-Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SLS_IMA-Player
+//
+//  Created by Carlos Ceja on 13/07/20.
+//  Copyright © 2020 Brightcove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
