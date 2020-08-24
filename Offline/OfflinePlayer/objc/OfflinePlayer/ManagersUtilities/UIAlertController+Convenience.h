4baab78af7de0e53c1fad88d8598e6b9afe3c7d9
Jeremy Blaker <jblaker@brightcove.com>
2020-08-24 16:57:32 -0400

    (OfflinePlayer) Allow for download retry after a failed download

--- Offline/OfflinePlayer/objc/OfflinePlayer/ManagersUtilities/UIAlertController+Convenience.h
+++ Offline/OfflinePlayer/objc/OfflinePlayer/ManagersUtilities/UIAlertController+Convenience.h
@@ -8,15 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface UIAlertController (Convenience)
 
 + (void)showAlertWithTitle:(NSString * _Nullable)title
                    message:(NSString * _Nullable)message
-               actionTitle:(NSString *)actionTitle
-              inController:(UIViewController *)controller;
+               actionTitle:(NSString * _Nullable)actionTitle
+              inController:(UIViewController * _Nullable)controller;
+
++ (void)showAlertWithTitle:(NSString * _Nullable)title
+                   message:(NSString * _Nullable)message
+               actionTitle:(NSString * _Nullable)actionTitle
+               cancelTitle:(NSString * _Nullable)cancelTitle
+               inController:(UIViewController * _Nullable)controller
+                 completion:(nullable void (^)(void))completionBlock;
 
 @end
-
-NS_ASSUME_NONNULL_END
--- Offline/OfflinePlayer/swift/OfflinePlayer/Helpers/UIAlertController+Helpers.swift
+++ Offline/OfflinePlayer/swift/OfflinePlayer/Helpers/UIAlertController+Helpers.swift
@@ -10,8 +10,21 @@ import UIKit
 extension UIAlertController {
     
     class func show(withTitle title: String, andMessage message: String) {
+        UIAlertController.show(withTitle: title, message: message, actionTitle: "OK", cancelTitle: nil, completion: nil)
+    }
+    
+    class func show(withTitle title: String, message: String, actionTitle: String, cancelTitle: String?, completion: (() -> Void)?) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: { (action: UIAlertAction) in
+            if let completion = completion {
+                completion()
+            }
+        }))
+        
+        if let cancelTitle = cancelTitle {
+            alert.addAction(UIAlertAction(title: cancelTitle, style: .cancel, handler: nil))
+        }
+        
         AppDelegate.current().tabBarController.present(alert, animated: true, completion: nil)
     }
     
