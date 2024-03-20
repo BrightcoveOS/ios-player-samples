eb8ae50db85224177206ed04e717bc861d154446
Carlos Ceja Limón <cceja@brightcove.com>
2024-03-20 15:16:39 -0600

    SidecarSubtitles

--- SidecarSubtitles/BasicSidecarSubtitlesPlayer-iOS/objc/BasicSidecarSubtitlesPlayer/ViewController.h
+++ SidecarSubtitles/BasicSidecarSubtitlesPlayer-iOS/objc/BasicSidecarSubtitlesPlayer/ViewController.h
@@ -2,13 +2,12 @@
 //  ViewController.h
 //  BasicSidecarSubtitlesPlayer
 //
-//  Copyright © 2020 Brightcove, Inc. All rights reserved.
-//  License: https://accounts.brightcove.com/en/terms-and-conditions
+//  Copyright © 2024 Brightcove, Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
+
 @interface ViewController : UIViewController
 
 @end
-
